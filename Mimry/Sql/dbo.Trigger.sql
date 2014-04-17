5851ebd8e8119f4dc7f0615a5e7351859dd717a6
Luong Hoang <lhoang@live.com>
2014-04-16 23:53:41 -0400

    - Add more sql triggers - Updated todo

--- Mimry/Sql/dbo.Trigger.sql
+++ Mimry/Sql/dbo.Trigger.sql
@@ -17,10 +17,33 @@ CREATE TRIGGER [UpdateCreatedDateTriggerMim]
 	END
 
 CREATE TRIGGER [UpdateCreatedDateTriggerMimSeq]
-	ON [dbo].[Mims]
+	ON [dbo].[MimSeqs]
 	AFTER INSERT
 	AS
 	BEGIN
 		SET NOCOUNT ON
-		UPDATE [dbo].[Mims] Set CreatedDate = GetDate() where MimID in (SELECT MimID FROM Inserted)
+		UPDATE [dbo].[MimSeqs] Set CreatedDate = GetDate() where MimSeqID in (SELECT MimSeqID FROM Inserted)
 	END
+
+SELECT 
+     sysobjects.name AS trigger_name 
+    ,USER_NAME(sysobjects.uid) AS trigger_owner 
+    ,s.name AS table_schema 
+    ,OBJECT_NAME(parent_obj) AS table_name 
+    ,OBJECTPROPERTY( id, 'ExecIsUpdateTrigger') AS isupdate 
+    ,OBJECTPROPERTY( id, 'ExecIsDeleteTrigger') AS isdelete 
+    ,OBJECTPROPERTY( id, 'ExecIsInsertTrigger') AS isinsert 
+    ,OBJECTPROPERTY( id, 'ExecIsAfterTrigger') AS isafter 
+    ,OBJECTPROPERTY( id, 'ExecIsInsteadOfTrigger') AS isinsteadof 
+    ,OBJECTPROPERTY(id, 'ExecIsTriggerDisabled') AS [disabled] 
+FROM sysobjects 
+/*
+INNER JOIN sysusers 
+    ON sysobjects.uid = sysusers.uid 
+*/  
+INNER JOIN sys.tables t 
+    ON sysobjects.parent_obj = t.object_id 
+
+INNER JOIN sys.schemas s 
+    ON t.schema_id = s.schema_id 
+WHERE sysobjects.type = 'TR' 
