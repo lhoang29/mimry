879d79759e574fa18b55b2ab8e646d0424c2d808
Luong Hoang <lhoang@live.com>
2014-04-16 22:40:48 -0400

    Updated code to add trigger for CreatedDate for both tables

--- Mimry/Sql/dbo.Trigger.sql
+++ Mimry/Sql/dbo.Trigger.sql
@@ -6,3 +6,21 @@
 		SET NOCOUNT ON
 		UPDATE [dbo].[Mims] Set LastModifiedDate = GetDate() where MimID in (SELECT MimID FROM Inserted)
 	END
+
+CREATE TRIGGER [UpdateCreatedDateTriggerMim]
+	ON [dbo].[Mims]
+	AFTER INSERT
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		UPDATE [dbo].[Mims] Set CreatedDate = GetDate() where MimID in (SELECT MimID FROM Inserted)
+	END
+
+CREATE TRIGGER [UpdateCreatedDateTriggerMimSeq]
+	ON [dbo].[Mims]
+	AFTER INSERT
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		UPDATE [dbo].[Mims] Set CreatedDate = GetDate() where MimID in (SELECT MimID FROM Inserted)
+	END
