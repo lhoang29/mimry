94910c37efb0c6335b924d5a954061e691dd4166
Luong Hoang <lhoang@live.com>
2014-04-26 10:46:26 -0400

    - Include Font Awesome in the project instead of referencing url

--- Mimry/App_Start/BundleConfig.cs
+++ Mimry/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Mimry
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
