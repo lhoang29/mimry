9ed34fc0238d71d86399c43f6b839a977ddd7681
Luong Hoang <lhoang@live.com>
2014-05-08 14:51:41 -0400

    Implemented unobtrusive javascript for like and vote actions

--- Mimry/Scripts/Site.js
+++ Mimry/Scripts/Site.js
@@ -1,49 +1,61 @@
-﻿function likeMimry(mimryID) {
-    $this = $(this);
-    $.ajax({
-        url: '/MimSeqs/Like/' + mimryID,
-        type: "POST",
-        dataType: "json",
-        cache: false,
-        success: function (data) {
-            if (data == 'success') {
-                var currentlyLiked = $this.hasClass('ml-liked');
-                $this.attr('class', currentlyLiked ? 'ml' : 'ml-liked');
-                $this.attr('title', currentlyLiked ? 'Like' : 'Liked');
-            }
-            else {
-                window.location = data;
-            }
+﻿$(function () {
+    voteMim = function (mimryID, vote) {
+        if ($(this).hasClass('ml-liked')) {
+            vote = 0;
         }
-    });
-}
-
-function voteMim(mimryID, vote) {
-    if ($(this).hasClass('ml-liked')) {
-        vote = 0;
-    }
-    $this = $(this);
-    $.ajax({
-        url: '/Mims/Vote/' + mimryID + '?vote=' + vote,
-        type: "POST",
-        dataType: "json",
-        cache: false,
-        success: function (data) {
-            if (data == 'success') {
-                if (vote != 0) {
-                    $this.attr('class', 'ml-liked');
+        $this = $(this);
+        $.ajax({
+            url: '/Mims/Vote/' + mimryID + '?vote=' + vote,
+            type: "POST",
+            dataType: "json",
+            cache: false,
+            success: function (data) {
+                if (data == 'success') {
+                    if (vote != 0) {
+                        $this.attr('class', 'ml-liked');
+                    }
+                    else {
+                        $this.attr('class', 'ml');
+                    }
+                    $this.siblings('.ml-liked').attr('class', 'ml');
                 }
                 else {
-                    $this.attr('class', 'ml');
+                    window.location = data;
                 }
-                $this.siblings('.ml-liked').attr('class', 'ml');
             }
-            else {
-                window.location = data;
-            }
-        }
+        });
+    }
+
+    $('.ajVoteUp').click(function () {
+        voteMim.call(this, $(this).siblings('[name="MimID"]').val(), 1);
     });
-}
+    $('.ajVoteDown').click(function () {
+        voteMim.call(this, $(this).siblings('[name="MimID"]').val(), -1);
+    });
+
+    likeMimry = function (mimryID) {
+        $this = $(this);
+        $.ajax({
+            url: '/MimSeqs/Like/' + mimryID,
+            type: "POST",
+            dataType: "json",
+            cache: false,
+            success: function (data) {
+                if (data == 'success') {
+                    var currentlyLiked = $this.hasClass('ml-liked');
+                    $this.attr('class', currentlyLiked ? 'ml' : 'ml-liked');
+                    $this.attr('title', currentlyLiked ? 'Like' : 'Liked');
+                }
+                else {
+                    window.location = data;
+                }
+            }
+        });
+    }
+    $('.ajLike').click(function () {
+        likeMimry.call(this, $(this).siblings('[name="MimSeqID"]').val());
+    });
+});
 
 function clearComment() {
     $('#txtComment').val('');
