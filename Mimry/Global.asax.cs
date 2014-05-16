7a169a4456ea24abd2b4372c15af11d7a763b282
Luong Hoang <lhoang@live.com>
2014-05-16 14:25:18 -0400

    - Added mini-profiler for both regular code and sql query. This is   currently using Mini profiler EF6 beta version. The profiling is   currently turned on only local requests.

--- Mimry/Global.asax.cs
+++ Mimry/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
+using StackExchange.Profiling.EntityFramework6;
 
 namespace Mimry
 {
@@ -16,6 +18,18 @@ namespace Mimry
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MiniProfilerEF6.Initialize();
+        }
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
