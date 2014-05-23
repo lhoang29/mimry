80f5519c3b091a46dd4c844b338992948fca383f
Luong Hoang <lhoang@live.com>
2014-05-23 14:13:50 -0400

    Set max-age cache on image response

--- Mimry/Controllers/MimsController.cs
+++ Mimry/Controllers/MimsController.cs
@@ -160,6 +160,7 @@ namespace Mimry.Controllers
 
             Response.Cache.SetCacheability(HttpCacheability.Public);
             Response.Cache.SetLastModified(mim.LastModifiedDate);
+            Response.Cache.SetMaxAge(TimeSpan.FromDays(1));
 
             return base.File(imageData, contentType);
         }
