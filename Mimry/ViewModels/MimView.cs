308461cdbce6e2fa788b72987504d41d89558db0
Luong Hoang <lhoang@live.com>
2014-05-30 12:45:24 -0400

    - Added an "Add" box at the end of each Mimry. - Fixed bug in MimSeqs/Add cancel button that doesn't do anything if no   returnUrl is specified. Changed this to default to details view.

--- Mimry/ViewModels/MimView.cs
+++ Mimry/ViewModels/MimView.cs
@@ -12,11 +12,15 @@ namespace Mimry.ViewModels
     }
     public class MimView
     {
+        public Guid MimSeqID { get; set; }
         public Guid MimID { get; set; }
         public int Vote { get; set; }
         public MimViewMode ViewMode { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public static readonly int ContainerPadding = 20;
+        public static readonly int AddViewSize = 250;
 
         private const int c_ThumbnailSize = 350;
         private const int c_MediumSize = 600;
