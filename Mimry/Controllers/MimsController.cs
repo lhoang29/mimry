f97efe922cb3630279c33ac5ff1de0256f074530
Luong Hoang <lhoang@live.com>
2014-05-30 16:40:47 -0400

    Removed max-age setting from image

--- Mimry/Controllers/MimsController.cs
+++ Mimry/Controllers/MimsController.cs
@@ -149,7 +149,6 @@ namespace Mimry.Controllers
 
             Response.Cache.SetCacheability(HttpCacheability.Public);
             Response.Cache.SetLastModified(mim.LastModifiedDate);
-            Response.Cache.SetMaxAge(TimeSpan.FromDays(60));
 
             return base.File(imageData, contentType);
         }
