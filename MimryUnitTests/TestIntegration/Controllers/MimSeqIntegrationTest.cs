2398b82b26e2f9106d1e516177c32821ef5f108d
Luong Hoang <lhoang@live.com>
2014-06-03 20:16:49 -0400

    Added more tests for MimSeqs/About and MimSeqs/Index view

--- MimryUnitTests/TestIntegration/Controllers/MimSeqIntegrationTest.cs
+++ MimryUnitTests/TestIntegration/Controllers/MimSeqIntegrationTest.cs
@@ -16,5 +16,19 @@ namespace MimryUnitTests.Controllers
             App.NavigateTo<MimSeqsController>(c => c.Create(returnUrl));
             BaseIntegrationTest.TestRouteMatch(App.Route, "Account", "Login");
         }
+
+        [TestMethod]
+        public void MimSeqIndex_WithoutSession_NoRedirect()
+        {
+            App.NavigateTo<MimSeqsController>(c => c.Index(0));
+            BaseIntegrationTest.TestRouteMatch(App.Route, "MimSeqs", "Index");
+        }
+
+        [TestMethod]
+        public void MimSeqAbout_WithoutSession_NoRedirect()
+        {
+            App.NavigateTo<MimSeqsController>(c => c.About());
+            BaseIntegrationTest.TestRouteMatch(App.Route, "MimSeqs", "About");
+        }
     }
 }
