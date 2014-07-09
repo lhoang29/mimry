fb13f202a8387b718ee4d15a46f9982f5b992cd9
Luong Hoang <lhoang@live.com>
2014-07-08 22:09:06 -0400

    - removed unused css style for div.mim - added xml docs to mimview class methods - modified memes in medium mode to display at the same width for uniform   look. minimum and maximum width are enforced.

--- Mimry/ViewModels/MimView.cs
+++ Mimry/ViewModels/MimView.cs
@@ -22,8 +22,23 @@ namespace Mimry.ViewModels
         public static readonly int ContainerPadding = 20;
         public static readonly int AddViewSize = 250;
 
+        private const int c_MinimumSize = 300;
         private const int c_ThumbnailSize = 350;
         private const int c_MediumSize = 600;
+
+        /// <summary>
+        /// The universal minimum size to display a meme regardless of its display mode.
+        /// </summary>
+        public static int MinimumSize
+        {
+            get { return c_MinimumSize; }
+        } 
+
+        /// <summary>
+        /// Gets the maximum size of a meme for the specified view mode.
+        /// </summary>
+        /// <param name="mode">The meme view mode.</param>
+        /// <returns>Maximum size (in pixels).</returns>
         public static int GetMaxMimSize(MimViewMode mode)
         {
             int maxSize = 0;
@@ -39,6 +54,11 @@ namespace Mimry.ViewModels
             return maxSize;
         }
 
+        /// <summary>
+        /// Gets the meme container size for the specified view mode.
+        /// </summary>
+        /// <param name="mode">The meme view mode.</param>
+        /// <returns>The size of the container (in pixels).</returns>
         public static int GetContainerSize(MimViewMode mode)
         {
             return MimView.GetMaxMimSize(mode) + MimView.ContainerPadding;
