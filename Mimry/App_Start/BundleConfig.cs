c052d393c73672a1740f61a6cc1aad4c6dd1e850
Luong Hoang <lhoang@live.com>
2014-08-26 22:42:06 -0400

    supported stacking images vertically in the studio

--- Mimry/App_Start/BundleConfig.cs
+++ Mimry/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Mimry
             bundles.Add(new ScriptBundle("~/bundles/bootbox").Include(
                         "~/Scripts/bootbox*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/studio").Include(
+                        "~/Scripts/studio.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Mimry/Scripts/studio.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var canvas = new fabric.Canvas('cvPreview');
+
+    $('#memeUrl').on('input', function (e) {
+        $("<img>", {
+            src: $(this).val(),
+            load: function () {
+                var scale = canvas.width / this.width;
+                var imgInstance = new fabric.Image(this);
+                imgInstance.scale(scale);
+                imgInstance.selectable = false;
+
+                var controls = canvas.getObjects();
+                var bottom = 0;
+                for (var i = 0; i < controls.length; i++) {
+                    if (controls[i] instanceof fabric.Image) {
+                        if (bottom < controls[i].top + controls[i].currentHeight) {
+                            bottom = controls[i].top + controls[i].currentHeight;
+                        }
+                    }
+                }
+                imgInstance.top = bottom;
+                
+                canvas.add(imgInstance);
+
+                // Adjust canvas height
+                canvas.setHeight(bottom + imgInstance.currentHeight);
+            }
+        });
+    });
+});
